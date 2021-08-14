7620543154a63125aa6ed26f8c512714beb943e5
Junhua <wenjh1998@gmail.com>
2021-08-14 23:46:47 +0800

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -2,19 +2,20 @@ import os
 from main import run, tokenizer
 
 
-test_dir = os.path.join(".", 'tests')
-for filename in os.listdir(test_dir):
-    full_path = os.path.join(test_dir, filename)
-    
-    try: 
-        print(f"Running tests on {full_path}")
-        run(full_path)
-        print(f"Passed")
-    except Exception as e:
-        print("Failed")
-        try:
-            for item in tokenizer(full_path):
-                print(item)
-        except:
-            print("Lexor issue")
-        print(f"Error encountered in {full_path}: {e.with_traceback(None)}")
+if __name__ == "__main__":
+    test_dir = os.path.join(".", 'tests')
+    for filename in os.listdir(test_dir):
+        full_path = os.path.join(test_dir, filename)
+        
+        try: 
+            print(f"Running tests on {full_path}")
+            run(full_path)
+            print(f"Passed")
+        except Exception as e:
+            print("Failed")
+            try:
+                for item in tokenizer(full_path):
+                    print(item)
+            except:
+                print("Lexor issue")
+            print(f"Error encountered in {full_path}: {e.with_traceback(None)}")    
