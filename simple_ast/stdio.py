b1caed3ed5ecbdd95c0babffccb7cac7c4ca89bb
Junhua <wenjh1998@gmail.com>
2021-08-14 23:55:56 +0800

    Update stdio.py

--- simple_ast/stdio.py
+++ simple_ast/stdio.py
@@ -1,16 +1,18 @@
 from . import BaseBox
 
+
 class Print(BaseBox):
     def __init__(self, value) -> None:
         self.value = value
-    
+
     def eval(self, context):
         print(self.value.eval(context))
+
 
 class Read(BaseBox):
     def __init__(self, value) -> None:
         self.value = value
-    
+
     def eval(self, context):
         varname = self.value.eval(context)
         context.vars[varname] = int(input())
