6ba5ff128d167e1c6534ff8bdb117f6c7f64bbfe
Junhua <wenjh1998@gmail.com>
2021-08-14 23:45:06 +0800

    Update main

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ from lexer import lexer
 from simp_parser import parser as simpleparser
 from interpreter import Interpreter
 
-def tokenizer(filename):
+def readFile(filename):
     with open(filename) as file:
-        data = file.read()
+        return file.read()
+
+def tokenizer(filename):
+    """Only tokenise the simple file"""
+    data = readFile(filename)
     tokens = lexer.lex(data)
     return [i for i in tokens]
 
 
 def run(filename: str):
-    with open(filename) as file:
-        data = file.read()
+    """Run the simple file"""
+    data = readFile(filename)
+
     tokens = lexer.lex(data)
     results = simpleparser.parse(tokens)
     intepreter = Interpreter(results)
@@ -21,7 +26,7 @@ def run(filename: str):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("source", type=str)
+    parser.add_argument("source", type=str, help="Path to source file")
 
     args = parser.parse_args()
     source = args.source
