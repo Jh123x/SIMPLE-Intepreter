02b3de15766014b9bef330656e4de77684f18a34
Junhua <wenjh1998@gmail.com>
2021-08-14 23:47:12 +0800

    Remove stack as it is not required

--- interpreter.py
+++ interpreter.py
@@ -1,12 +1,10 @@
 class Context(object):
     def __init__(self):
-        self.stack = []
         self.vars = {}
         self.funcs = {}
 
 
 class Interpreter(object):
-
     def __init__(self, parse_result):
         self.parse_result = parse_result
         self.context = Context()
